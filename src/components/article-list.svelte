4d4b1c35e100c08e54c57bb9c7e5fe60e3fd5a13
geonya <geony8410@gmail.com>
2023-01-26 08:51:43 +0900

    html/css setup

---
+++ src/components/article-list.svelte
@@ -0,0 +1,11 @@
+<script>
+  import ArticleEditForm from "./article-edit-form.svelte";
+  import Article from "./article.svelte";
+</script>
+
+<!-- start article-wrap -->
+<div class="articles-wrap">
+  <Article />
+  <ArticleEditForm />
+</div>
+<!-- end article-wrap -->
